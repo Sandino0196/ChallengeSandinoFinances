697e37a6d3df80c4165956bda26dc251d8750780
Sandino <sandinorodrigo@gmail.com>
2020-07-21 19:13:50 -0600

    Some Controllers and DB

---
+++ ChallengeSandinoFinances/Controllers/Expense_DetailController.cs
@@ -0,0 +1,73 @@
+﻿using ChallengeSandinoFinances.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChallengeSandinoFinances.Controllers
+{
+    public class Expense_DetailController : ApiController
+    {
+        public Expense_DetailController()
+        {
+
+        }
+
+        // GET: api/Expense_Detail
+        public IEnumerable<Expense_Detail> Get()
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                return entities.Expense_Detail.ToList();
+            }
+        }
+
+        // GET: api/Expense_Detail/5
+        public Expense_Detail Get(int id)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                return entities.Expense_Detail.FirstOrDefault(e => e.ID_Expense_Detail == id);
+            }
+        }
+
+        // POST: api/Expense_Detail
+        public void Post([FromBody]Expense_Detail value)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                entities.Expense_Detail.Add(value);
+                entities.SaveChanges();
+            }
+        }
+
+        // PUT: api/Expense_Detail/5
+        public void Put(int id, [FromBody]Expense_Detail value)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                if(value.ID_Expense_Detail == id)
+                {
+                    entities.Entry(value).State = System.Data.Entity.EntityState.Modified;
+                    entities.SaveChanges();
+                }                
+            }
+        }
+
+        // DELETE: api/Expense_Detail/5
+        public void Delete(int id)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                var temp = entities.Expense_Detail.FirstOrDefault(e => e.ID_Expense_Detail == id);
+                if (temp.ID_Expense_Detail == id)
+                {
+                    entities.Expense_Detail.Remove(temp);
+                    entities.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ ChallengeSandinoFinances/Controllers/ExpensesController.cs
@@ -0,0 +1,73 @@
+﻿using ChallengeSandinoFinances.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChallengeSandinoFinances.Controllers
+{
+    public class ExpensesController : ApiController
+    {
+        public ExpensesController()
+        {
+
+        }
+
+        // GET: api/Expenses
+        public IEnumerable<Expenses> Get()
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                return entities.Expenses.ToList();
+            }
+        }
+
+        // GET: api/Expenses/5
+        public Expenses Get(int id)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                return entities.Expenses.FirstOrDefault(e => e.ID_Expense == id);
+            }
+        }
+
+        // POST: api/Expenses
+        public void Post([FromBody]Expenses value)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                entities.Expenses.Add(value);
+                entities.SaveChanges();
+            }
+        }
+
+        // PUT: api/Expenses/5
+        public void Put(int id, [FromBody]Expenses value)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                if(value.ID_Expense == id)
+                {
+                    entities.Entry(value).State = System.Data.Entity.EntityState.Modified;
+                    entities.SaveChanges();
+                }                
+            }
+        }
+
+        // DELETE: api/Expenses/5
+        public void Delete(int id)
+        {
+            using (FinancesChallengeDBEntities entities = new FinancesChallengeDBEntities())
+            {
+                var temp = entities.Expenses.FirstOrDefault(e => e.ID_Expense == id);
+                if (temp.ID_Expense == id)
+                {
+                    entities.Expenses.Remove(temp);
+                    entities.SaveChanges();
+                }
+            }
+        }
+    }
+}
